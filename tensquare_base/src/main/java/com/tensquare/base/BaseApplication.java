2d09acb9c348c46ed5bce6e5b1d956178a67e32a
Mrchai521 <18636902115@163.com>
2020-08-28 10:53:46 +0800

    增加基本base模块

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.tensquare.base;
+
+import com.tensquare.utils.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @description: 基础服务的启动类
+ * @author:柴新峰
+ * @create:2020/8/28
+ */
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+}

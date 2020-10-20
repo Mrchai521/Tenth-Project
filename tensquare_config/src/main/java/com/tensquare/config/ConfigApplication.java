5e6d9c187d95c2ef15d145205fa4b1c348eb6a41
Mrchai521 <18636902115@163.com>
2020-10-20 16:39:53 +0800

    增加网关总线模块

---
+++ tensquare_config/src/main/java/com/tensquare/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @description:
+ * @author:柴新峰
+ * @create:2020/10/20
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
